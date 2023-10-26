d52dec9c3382473764fede7e76e61f4ac19e386f
Pratikshit Singh <pratikshit.singh.eee16@itbhu.ac.in>
2023-10-25 20:33:37 -0500

    chaned scss to css

--- webpack.config.js
+++ webpack.config.js
@@ -3,48 +3,48 @@ const CopyPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
-  entry: "./index.js",
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, "dist"),
-    publicPath: '/'
-  },
-  devServer: {
-    open: true,
-    host: "localhost",
-    watchFiles: 'index.html',
-  },
-  context: path.join(__dirname, 'docs'),
-  plugins: [
-    new CopyPlugin({
-      patterns: [
-        { from: './assets/', to: './assets/' },
-      ],
-    }),
-    new HtmlWebpackPlugin({
-      template: "index.html",
-      inject: 'body',
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/i,
-        loader: "babel-loader",
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: "asset",
-      },
-      {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
+    mode: "development",
+    entry: "./index.js",
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, "dist"),
+        publicPath: '/'
+    },
+    devServer: {
+        open: true,
+        host: "localhost",
+        watchFiles: 'index.html',
+    },
+    context: path.join(__dirname, 'docs'),
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { from: './assets/', to: './assets/' },
+            ],
+        }),
+        new HtmlWebpackPlugin({
+            template: "index.html",
+            inject: 'body',
+        }),
     ],
-  },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/i,
+                loader: "babel-loader",
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                type: "asset",
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+        ],
+    },
 };
